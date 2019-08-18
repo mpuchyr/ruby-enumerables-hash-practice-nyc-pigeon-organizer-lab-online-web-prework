125d08e21ce9a04f835f8778254519aa7d6e776c
Matt Puchyr <matt.puchyr@gmail.com>
2019-08-18 19:53:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,5 +2,15 @@ def nyc_pigeon_organizer(data)
   pigeon_names = []
   final_pigeon_list = {}
   
+  data.reduce({}) do |memo, (key, value)|
+    value.reduce({}) do |memo2, (key2, value2)|
+      value2.each do |item|
+        pigeon_names.push(item)
+      end
+    end
+  end
+  
+  pigeon_names = pigeon_names.uniq
+  
   
 end
